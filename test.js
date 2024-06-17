eaf0d2088742d58ec4a5419dbbcf8cfc86cfc109
11-crypto <shubhamdobhal61@gmail.com>
2024-06-17 16:10:57 +0000

    inital commit

---
+++ test.js
@@ -0,0 +1,10 @@
+for(var i=1;i<=12;i++){ //loop
+   // console.log(" X"+i+" = "+i*2)
+   // console.log("2 X "+i+" = "+i*2)  //printing table of 2
+}
+
+
+//integer
+//float/decimal
+//charcter a b c ? . ! @ 1  2  4
+//String " " '
