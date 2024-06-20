c0b52b21b3213eb4e5ca549b3eceb0b7d8e8120e
11-crypto <shubhamdobhal61@gmail.com>
2024-06-20 10:11:38 +0000

    config done

---
+++ 01_basics/02_datatypes.js
@@ -0,0 +1,34 @@
+"use strict" // treat all js code as newer version
+
+console.log(3+3) 
+
+// alert(3+3) // we are uding node js not browser
+
+console.log(3+3) ; console.log("shubham")
+
+// code readability is important //
+
+console.log(3+3)
+
+console.log("shubham")
+
+let name = ("shubham")
+let age = 3
+//let isLoggedIn = True/False;
+
+// number => 2 to the power of 53
+// bigint
+
+// string => ""
+// boolean => True/False
+// null => standalone value   // null is a special datatype
+// undefined
+// symbol  // for unique
+
+
+// object
+
+console.log(typeof "hitesh");
+console.log(typeof age)
+console.log(typeof null)
+console.log(typeof undefined)
