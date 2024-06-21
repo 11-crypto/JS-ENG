26a9ea35e38905a7e97ad0d57afadf40b38f54d8
11-crypto <shubhamdobhal61@gmail.com>
2024-06-21 15:13:42 +0000

    config done

---
+++ 01_basics/03_conversionoperation.js
@@ -0,0 +1,24 @@
+let score = "shubham"
+
+// score = 33 = number , null = 0  , true = 1 , false = 0 , "shubham" = nan , "33abc" = NaN //
+console.log(typeof score)
+console.log(typeof (score) )
+
+let valueInNumber = Number (score)
+console.log(typeof valueInNumber);
+console.log(valueInNumber)
+// true => 1
+
+let isLoggedIn = "33"
+
+let booleanIsLoggedIn = Boolean(isLoggedIn)
+//console.log(booleanIsLoggedIn)
+
+// 1 => true , 0 = false
+// "" => false , "shubham" = true //
+
+let someNumber = 33
+
+let stringNumber = String(someNumber)
+console.log(stringNumber);
+console.log(typeof stringNumber);
